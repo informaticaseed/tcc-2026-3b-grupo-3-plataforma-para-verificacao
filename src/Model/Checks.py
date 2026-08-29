44efb46de4fd5091dca7d93b70e465e2ffb9f5ab
Gabriel José <264185595+gelinho224-spec@users.noreply.github.com>
2026-08-29 01:31:13 -0300

    Melhorias na verificação de HTTP/HTTPS

--- src/Model/Checks.py
+++ src/Model/Checks.py
@@ -10,10 +10,15 @@ def check_length(url):
     
 def check_http(url):
 #O https é seguro e encriptado, já o http não.
-    if url.startswith("https"):
+    scheme = urlparse(url).scheme.lower()
+
+    if scheme == "https":
         return 0, "O URL utiliza HTTPS"
-    else:
+    elif scheme == "http":
         return 20, "O URL utiliza HTTP"
+    else:
+        return 20, "O URL não utiliza HTTP/HTTPS"
+    
     
 def check_at_symbol(url):
 #O @ pode ser utilizado para esconder o dominio real do site, sendo utilizado em links de phishing.
@@ -23,7 +28,7 @@ def check_at_symbol(url):
         return 0, "O URL não possui um @"
 
 def check_ip(url):
-    # URLs que utilizam um endereço IP tem mais chance de ser phishing(Não garante pois sites de roteadores utilizam ipv4 como "link")
+    # URLs que utilizam um endereço IP como dominio tem mais chance de ser phishing(Não garante pois sites de roteadores utilizam ipv4 como "link")
     hostname = urlparse(url).hostname
 
     if hostname is None:
@@ -34,7 +39,7 @@ def check_ip(url):
     if len(parts) == 4 and all(part.isnumeric() for part in parts):
         return 20, "O URL utiliza um endereço IP"
 
-    return 0, "O URL utiliza um domínio"
+    return 0, "O URL utiliza um domínio que pareçe seguro"
 
 
 def analisar_url(url):
