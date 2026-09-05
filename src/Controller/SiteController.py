74f7295b226a4819057d48521ac13576b5f4a915
Gabriel José <264185595+gelinho224-spec@users.noreply.github.com>
2026-09-05 11:01:47 -0300

    Adicionado frontend incial para testes mais concretos.

--- src/Controller/SiteController.py
+++ src/Controller/SiteController.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel, HttpUrl
 from src.Model.Checks import analisar_url
 from src.View.main import resultado_analise, pagina_inicial
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 
 class URLCheckerRequest(BaseModel):
@@ -13,10 +15,28 @@ def criar_app():
     app = FastAPI()
 
 
-    @app.get("/")
-    def home():
 
-        return pagina_inicial()
+    app.mount(
+        "/static",
+        StaticFiles(directory="src/View/static"),
+        name="static"
+    )
+
+    print(app.routes)
+
+    templates = Jinja2Templates(
+        directory="src/View/templates"
+    )
+
+
+    @app.get("/")
+    def home(request: Request):
+
+        return templates.TemplateResponse(
+            request=request,
+            name="index.html"
+        )
+        
 
 
     @app.post("/check_url")
@@ -25,7 +45,7 @@ def criar_app():
         # Converte a URL recebida pelo Pydantic para string
         url = str(payload.url)
 
-        # Envia a URL para o Model
+      # Envia a URL para o Model
         score, reasons = analisar_url(url)
 
         # Envia o resultado para a View
